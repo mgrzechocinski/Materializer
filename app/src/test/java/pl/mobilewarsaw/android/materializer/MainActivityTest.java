26a19494c5e0a8eb78638b2cb11cf5d44a3b65fc
Mateusz Grzechociński <mateusz.grzechocinski@gmail.com>
2015-02-19 13:50:21 +0100

    Enable unit tests - just a dummy one

---
+++ app/src/test/java/pl/mobilewarsaw/android/materializer/MainActivityTest.java
@@ -0,0 +1,14 @@
+package pl.mobilewarsaw.android.materializer;
+
+import org.assertj.core.api.Assertions;
+import org.junit.Test;
+
+public class MainActivityTest {
+
+    @Test
+    public void shouldMainActivityBeNotNull() throws Exception {
+        //dummy test
+        MainActivity mainActivity = new MainActivity();
+        Assertions.assertThat(mainActivity).isNull();
+    }
+}
